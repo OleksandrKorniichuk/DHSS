d8b3f36b354506b6a70a74fac3faf1923e24aa28
Oleksandr Korniichuk <kov0249@gmail.com>
2014-09-14 20:05:07 +0300

    start

---
+++ scripts/index_scripts.js
@@ -0,0 +1,5 @@
+setTimeout("StartIndex()", 1500);
+function StartIndex()
+{
+    window.onload = window.location.assign("Lobby.html");  
+}
---
+++ scripts/lobby_scripts.js
@@ -0,0 +1,8 @@
+function openbox(id){
+    display = document.getElementById('menu_list').style.display; 
+    if(display=='none'){
+       document.getElementById('menu_list').style.display='block';
+    }else{
+       document.getElementById('menu_list').style.display='none';
+    }
+}
